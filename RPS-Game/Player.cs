5ec98eee9f12f86946f3315af7d676c94c9a87b3
abdallah abuhalima <abdallahabuhalima.1.jo@gmail.com>
2024-06-24 20:56:11 +0300

    whole project added

---
+++ RPS-Game/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPS_Game
+{
+    public class Player
+    {
+        private string name;
+        private int score;
+        public Player(string name)
+        {
+            this.name = name;
+            score = 0;
+        }
+        public string chooseMove()
+        {
+            Console.WriteLine("Make your move: ");
+            string move=Console.ReadLine();
+            while (move.ToLower() != "rock" && move.ToLower() != "paper" && move.ToLower() != "scissors")
+            {
+                Console.WriteLine("You didn't make a valid move, please enter a valid move: ");
+                move = Console.ReadLine();
+            }
+            return move.ToLower();
+        }
+        public void setPlayerScore(int score)
+        {
+            this.score = score;
+        }
+        public int getPlayerScore()
+        {
+            return this.score;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+    }
+}
